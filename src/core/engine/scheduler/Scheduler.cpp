a5f1159b3d73d02e5237d30dd450fa239f61e04e
Aphile <taiko6661@gmail.com>
2025-08-22 13:42:19 +0500

    Да

---
+++ src/core/engine/scheduler/Scheduler.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Aphile on 22.08.2025.
+//
+
+#include "Scheduler.h"
+
+namespace Scheduler {
+} // Scheduler
---
+++ src/core/engine/scheduler/Scheduler.h
@@ -0,0 +1,16 @@
+//
+// Created by Aphile on 22.08.2025.
+//
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+namespace Scheduler {
+
+class Scheduler {
+
+};
+
+} // Scheduler
+
+#endif //SCHEDULER_H
